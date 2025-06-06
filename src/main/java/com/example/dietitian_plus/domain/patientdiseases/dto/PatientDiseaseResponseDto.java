d0367a21979ef19d3c6185349b91fa2a7c679ea8
JagodaJakub <kubajagjag@gmail.com>
2025-06-06 04:14:11 +0200

    Remove unnecessary import

--- src/main/java/com/example/dietitian_plus/domain/patientdiseases/dto/PatientDiseaseResponseDto.java
+++ src/main/java/com/example/dietitian_plus/domain/patientdiseases/dto/PatientDiseaseResponseDto.java
@@ -1,7 +1,6 @@
 package com.example.dietitian_plus.domain.patientdiseases.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
