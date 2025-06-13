1125117ac9468519df6e6dc0137690e66cf5e7b6
JagodaJakub <kubajagjag@gmail.com>
2025-06-13 14:30:36 +0200

    Replace constructor with Lombok RequiredArgsConstructor annotation

--- src/main/java/com/example/dietitian_plus/domain/dishesproducts/dto/DishProductDtoMapper.java
+++ src/main/java/com/example/dietitian_plus/domain/dishesproducts/dto/DishProductDtoMapper.java
@@ -3,21 +3,18 @@ package com.example.dietitian_plus.domain.dishesproducts.dto;
 import com.example.dietitian_plus.domain.dishesproducts.DishProduct;
 import com.example.dietitian_plus.domain.product.dto.ProductDtoMapper;
 import com.example.dietitian_plus.domain.unit.dto.UnitDtoMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class DishProductDtoMapper {
 
     private final ProductDtoMapper productDtoMapper;
     private final UnitDtoMapper unitDtoMapper;
-
-    public DishProductDtoMapper(ProductDtoMapper productDtoMapper, UnitDtoMapper unitDtoMapper) {
-        this.productDtoMapper = productDtoMapper;
-        this.unitDtoMapper = unitDtoMapper;
-    }
 
     public DishProductResponseDto toDto(DishProduct dishProduct) {
         DishProductResponseDto dto = new DishProductResponseDto();
