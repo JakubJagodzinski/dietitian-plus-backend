8f4efaa61b07012217b0b284c3447846308753d1
JagodaJakub <kubajagjag@gmail.com>
2025-06-17 22:59:43 +0200

    Add validation for password change to ensure fields are not null or blank

--- src/main/java/com/example/dietitian_plus/common/constants/messages/PasswordMessages.java
+++ src/main/java/com/example/dietitian_plus/common/constants/messages/PasswordMessages.java
@@ -5,5 +5,7 @@ public class PasswordMessages {
     public static final String WRONG_PASSWORD = "Wrong password";
     public static final String PASSWORDS_DONT_MATCH = "Passwords don't match";
     public static final String PASSWORD_CHANGED_SUCCESSFULLY = "Password changed successfully";
+    public static final String PASSWORD_CANT_BE_BLANK = "Password can't be blank";
+    public static final String SOME_FIELDS_ARE_NULL = "Some fields are null";
 
 }
