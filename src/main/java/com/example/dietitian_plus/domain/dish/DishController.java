dc3148c681c2a4ff45fca9f73c63840b4296b59f
JagodaJakub <kubajagjag@gmail.com>
2025-06-26 22:37:40 +0200

    Add OpenAPI annotations to DishController for improved API documentation

--- src/main/java/com/example/dietitian_plus/domain/dish/DishController.java
+++ src/main/java/com/example/dietitian_plus/domain/dish/DishController.java
@@ -4,7 +4,13 @@ import com.example.dietitian_plus.auth.access.CheckPermission;
 import com.example.dietitian_plus.domain.dish.dto.request.CreateDishRequestDto;
 import com.example.dietitian_plus.domain.dish.dto.request.UpdateDishRequestDto;
 import com.example.dietitian_plus.domain.dish.dto.response.DishResponseDto;
+import com.example.dietitian_plus.exception.ApiError;
 import com.example.dietitian_plus.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +30,25 @@ public class DishController {
 
     private final DishService dishService;
 
+    @Operation(summary = "Get all dishes")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of all dishes",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DishResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DISH_READ_ALL)
     @GetMapping("/dishes")
     public ResponseEntity<List<DishResponseDto>> getAllDishes() {
@@ -34,6 +59,17 @@ public class DishController {
                 .body(dishResponseDtoList);
     }
 
+    @Operation(summary = "Get all public dishes")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of all public dishes",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DishResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/dishes/public")
     public ResponseEntity<List<DishResponseDto>> getAllPublicDishes() {
         List<DishResponseDto> dishResponseDtoList = dishService.getAllPublicDishes();
@@ -43,6 +79,33 @@ public class DishController {
                 .body(dishResponseDtoList);
     }
 
+    @Operation(summary = "Get dish by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Dish found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DishResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Dish not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DISH_READ)
     @GetMapping("/dishes/{dishId}")
     public ResponseEntity<DishResponseDto> getDishById(@PathVariable Long dishId) {
@@ -53,6 +116,33 @@ public class DishController {
                 .body(dishResponseDto);
     }
 
+    @Operation(summary = "Get dietitian all dishes")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of dietitian all dishes",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DishResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Dietitian not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DIETITIAN_DISH_READ_ALL)
     @GetMapping("/dietitians/{dietitianId}/dishes")
     public ResponseEntity<List<DishResponseDto>> getDietitianAllDishes(@PathVariable UUID dietitianId) {
@@ -63,6 +153,33 @@ public class DishController {
                 .body(dishResponseDtoList);
     }
 
+    @Operation(summary = "Create new dish")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Dish created successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DishResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Dietitian not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DISH_CREATE)
     @PostMapping("/dishes")
     public ResponseEntity<DishResponseDto> createDish(@Valid @RequestBody CreateDishRequestDto createDishRequestDto) {
@@ -74,6 +191,33 @@ public class DishController {
                 .body(createdDishResponseDto);
     }
 
+    @Operation(summary = "Update dish by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Dish updated successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DishResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Dish not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DISH_UPDATE)
     @PatchMapping("/dishes/{dishId}")
     public ResponseEntity<DishResponseDto> updateDishById(@PathVariable Long dishId, @Valid @RequestBody UpdateDishRequestDto updateDishRequestDto) {
@@ -84,6 +228,29 @@ public class DishController {
                 .body(dishResponseDto);
     }
 
+    @Operation(summary = "Delete dish by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Dish deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Dish not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DISH_DELETE)
     @DeleteMapping("/dishes/{dishId}")
     public ResponseEntity<Void> deleteDishById(@PathVariable Long dishId) {
