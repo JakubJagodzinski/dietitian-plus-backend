378b98b90c100a12ec1427ac5cd8093469775814
JagodaJakub <kubajagjag@gmail.com>
2025-06-26 02:08:16 +0200

    Enhance Authentication and User Controllers with OpenAPI annotations for improved API documentation and error handling

--- src/main/java/com/example/dietitian_plus/auth/AuthenticationController.java
+++ src/main/java/com/example/dietitian_plus/auth/AuthenticationController.java
@@ -7,6 +7,12 @@ import com.example.dietitian_plus.auth.dto.response.AuthenticationResponseDto;
 import com.example.dietitian_plus.auth.dto.response.RefreshTokenResponseDto;
 import com.example.dietitian_plus.common.MessageResponseDto;
 import com.example.dietitian_plus.common.constants.messages.UserMessages;
+import com.example.dietitian_plus.exception.ApiError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +28,21 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @Operation(summary = "Register new account")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User registered successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Email is already taken",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<MessageResponseDto> register(@RequestBody RegisterRequestDto registerRequestDto) {
         service.register(registerRequestDto);
@@ -31,6 +52,21 @@ public class AuthenticationController {
                 .body(new MessageResponseDto(UserMessages.USER_CREATED_SUCCESSFULLY));
     }
 
+    @Operation(summary = "Authenticate with email and password")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User authenticated successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Wrong username or password",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody AuthenticationRequestDto authenticationRequestDto) {
         AuthenticationResponseDto authenticationResponseDto = service.authenticate(authenticationRequestDto);
@@ -40,6 +76,25 @@ public class AuthenticationController {
                 .body(authenticationResponseDto);
     }
 
+    @Operation(summary = "Use refresh token to generate new access and refresh token")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Token refreshed successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = RefreshTokenResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Provided refresh token is invalid or expired",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @PostMapping("/refresh-token")
     public ResponseEntity<RefreshTokenResponseDto> refreshToken(@RequestBody RefreshTokenRequestDto refreshTokenRequestDto) {
         RefreshTokenResponseDto refreshTokenResponseDto = service.refreshToken(refreshTokenRequestDto);
