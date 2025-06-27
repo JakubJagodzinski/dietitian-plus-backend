f884d9ea3cf077560c5fb5d087bc69e04e926b29
JagodaJakub <kubajagjag@gmail.com>
2025-06-27 14:13:33 +0200

    Add OpenAPI annotations to PatientDislikedProductController for improved API documentation

--- src/main/java/com/example/dietitian_plus/domain/patientdislikedproducts/PatientDislikedProductController.java
+++ src/main/java/com/example/dietitian_plus/domain/patientdislikedproducts/PatientDislikedProductController.java
@@ -4,7 +4,13 @@ import com.example.dietitian_plus.auth.access.CheckPermission;
 import com.example.dietitian_plus.domain.patientdislikedproducts.dto.request.AssignDislikedProductToPatientRequestDto;
 import com.example.dietitian_plus.domain.patientdislikedproducts.dto.response.PatientDislikedProductResponseDto;
 import com.example.dietitian_plus.domain.product.dto.response.ProductResponseDto;
+import com.example.dietitian_plus.exception.ApiError;
 import com.example.dietitian_plus.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +30,40 @@ public class PatientDislikedProductController {
 
     private final PatientDislikedProductService patientDislikedProductService;
 
+    @Operation(
+            summary = "Read patient all disliked products"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of patient all disliked products",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_DISLIKED_PRODUCT_READ_ALL)
     @GetMapping("/patients/{patientId}/disliked-products")
     public ResponseEntity<List<ProductResponseDto>> getPatientAllDislikedProducts(@PathVariable UUID patientId) {
@@ -34,6 +74,48 @@ public class PatientDislikedProductController {
                 .body(productResponseDtoList);
     }
 
+    @Operation(
+            summary = "Assign disliked product to patient"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Disliked product assigned to patient successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PatientDislikedProductResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Disliked product already assigned to patient",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient / product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_DISLIKED_PRODUCT_ASSIGN)
     @PostMapping("/patients/{patientId}/disliked-products")
     public ResponseEntity<PatientDislikedProductResponseDto> assignDislikedProductToPatient(@PathVariable UUID patientId, @Valid @RequestBody AssignDislikedProductToPatientRequestDto assignDislikedProductToPatientRequestDto) {
@@ -45,6 +127,44 @@ public class PatientDislikedProductController {
                 .body(createdPatientDislikedProductResponseDto);
     }
 
+    @Operation(
+            summary = "Unassign disliked product from patient"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Disliked product unassigned from patient successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Disliked product not assigned to patient",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient / product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_DISLIKED_PRODUCT_UNASSIGN)
     @DeleteMapping("/patients/{patientId}/disliked-products/{productId}")
     public ResponseEntity<Void> unassignDislikedProductFromPatient(@PathVariable UUID patientId, @PathVariable Long productId) {
