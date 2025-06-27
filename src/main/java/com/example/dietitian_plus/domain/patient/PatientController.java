d5e4a9ba980279cd60a0874efd705566dd0f480f
JagodaJakub <kubajagjag@gmail.com>
2025-06-27 15:02:46 +0200

    Add OpenAPI annotations to PatientController for improved API documentation

--- src/main/java/com/example/dietitian_plus/domain/patient/PatientController.java
+++ src/main/java/com/example/dietitian_plus/domain/patient/PatientController.java
@@ -8,7 +8,13 @@ import com.example.dietitian_plus.domain.patient.dto.request.PatientQuestionnair
 import com.example.dietitian_plus.domain.patient.dto.request.UpdatePatientRequestDto;
 import com.example.dietitian_plus.domain.patient.dto.response.PatientQuestionnaireStatusResponseDto;
 import com.example.dietitian_plus.domain.patient.dto.response.PatientResponseDto;
+import com.example.dietitian_plus.exception.ApiError;
 import com.example.dietitian_plus.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,6 +33,32 @@ public class PatientController {
 
     private final PatientService patientService;
 
+    @Operation(
+            summary = "Get all patients"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of all patients",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PatientResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_READ_ALL)
     @GetMapping("/patients")
     public ResponseEntity<List<PatientResponseDto>> getAllPatients() {
@@ -37,6 +69,40 @@ public class PatientController {
                 .body(patientResponseDtoList);
     }
 
+    @Operation(
+            summary = "Get patient by id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Patient found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PatientResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_READ)
     @GetMapping("/patients/{patientId}")
     public ResponseEntity<PatientResponseDto> getPatientById(@PathVariable UUID patientId) {
@@ -47,6 +113,40 @@ public class PatientController {
                 .body(patientResponseDto);
     }
 
+    @Operation(
+            summary = "Get patient questionnaire status"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Patient questionnaire status",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PatientResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_READ)
     @GetMapping("/patients/{patientId}/questionnaire-status")
     public ResponseEntity<PatientQuestionnaireStatusResponseDto> getPatientQuestionnaireStatus(@PathVariable UUID patientId) {
@@ -57,6 +157,41 @@ public class PatientController {
                 .body(patientQuestionnaireStatusResponseDto);
     }
 
+
+    @Operation(
+            summary = "Get dietitian all patients"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of dietitian all patients",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PatientResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Dietitian not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.DIETITIAN_PATIENT_READ_ALL)
     @GetMapping("/dietitians/{dietitianId}/patients")
     public ResponseEntity<List<PatientResponseDto>> getDietitianAllPatients(@PathVariable UUID dietitianId) {
@@ -67,6 +202,48 @@ public class PatientController {
                 .body(patientResponseDtoList);
     }
 
+    @Operation(
+            summary = "Fill patient questionnaire"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Questionnaire filled successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Patient questionnaire is already filled",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_UPDATE)
     @PostMapping("/patients/{patientId}/questionnaire")
     public ResponseEntity<MessageResponseDto> fillPatientQuestionnaire(@PathVariable UUID patientId, @Valid @RequestBody PatientQuestionnaireRequestDto patientQuestionnaireRequestDto) {
@@ -77,6 +254,40 @@ public class PatientController {
                 .body(new MessageResponseDto(PatientMessages.PATIENT_QUESTIONNAIRE_FILLED_SUCCESSFULLY));
     }
 
+    @Operation(
+            summary = "Update patient by id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Patient updated successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PatientResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_UPDATE)
     @PatchMapping("/patients/{patientId}")
     public ResponseEntity<PatientResponseDto> updatePatientById(@PathVariable UUID patientId, @Valid @RequestBody UpdatePatientRequestDto updatePatientRequestDto) {
@@ -87,6 +298,48 @@ public class PatientController {
                 .body(updatedPatientResponseDto);
     }
 
+    @Operation(
+            summary = "Assign dietitian to patient"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Dietitian assigned to patient successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Patient already has a dietitian assigned",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient / dietitian not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_DIETITIAN_ASSIGN)
     @PostMapping("/patients/dietitians")
     public ResponseEntity<MessageResponseDto> assignDietitianToPatient(@RequestParam(value = "patient_email") String patientEmail, @Valid @RequestBody AssignDietitianToPatientRequestDto assignDietitianToPatientRequestDto) {
@@ -97,6 +350,48 @@ public class PatientController {
                 .body(new MessageResponseDto("Dietitian with id " + assignDietitianToPatientRequestDto.getDietitianId() + " successfully assigned to patient with email " + patientEmail));
     }
 
+    @Operation(
+            summary = "Unassign dietitian from patient"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Dietitian unassigned from patient successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Dietitian is not assigned to this patient",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient / dietitian not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_DIETITIAN_UNASSIGN)
     @DeleteMapping("/patients/{patientId}/dietitians")
     public ResponseEntity<MessageResponseDto> unassignDietitianFromPatient(@PathVariable UUID patientId) {
@@ -107,6 +402,36 @@ public class PatientController {
                 .body(new MessageResponseDto("Dietitian successfully unassigned from patient with id " + patientId));
     }
 
+    @Operation(
+            summary = "Delete patient by id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Patient deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content()
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Patient not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PATIENT_DELETE)
     @DeleteMapping("/patients/{patientId}")
     public ResponseEntity<Void> deletePatientById(@PathVariable UUID patientId) {
